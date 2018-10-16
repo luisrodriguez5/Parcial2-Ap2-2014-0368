3b505fac9ea54f6cde49c77baaac713dc2ffb459
luisrodriguez5 <luis_rodriguez5@ucne.edu.do>
2018-10-16 18:48:49 -0400

    agregando Boostrap

---
+++ Ap2-Parcial2-Luis-Rodriguez/UI/RegistroPrestamos.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ap2_Parcial2_Luis_Rodriguez.UI
+{
+    public partial class RegistroPrestamos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
